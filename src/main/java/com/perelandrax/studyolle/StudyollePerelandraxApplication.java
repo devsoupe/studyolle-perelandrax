6e026f31d382464a636a798423841fbe3e2d6355
perelandrax <luvyum4e@gmail.com>
2023-01-20 23:59:33 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/perelandrax/studyolle/StudyollePerelandraxApplication.java
@@ -0,0 +1,13 @@
+package com.perelandrax.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyollePerelandraxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyollePerelandraxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perelandrax/studyolle/StudyollePerelandraxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perelandrax.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyollePerelandraxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
